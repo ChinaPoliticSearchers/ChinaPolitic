dfc0ff137dc779fa672e01e32fcde74e15f231fc
朱继慧 <11@11.com>
2016-11-22 11:11:01 +0800

    change sth about mongo_client

--- PoliticManager/src/data_proxy/mongodb/mongo_proxy.py
+++ PoliticManager/src/data_proxy/mongodb/mongo_proxy.py
@@ -1,12 +1,26 @@
+#coding=utf-8
 __author__ = 'friddle'
 
 from pymongo import MongoClient
 from common.basic_proxy import  BasicDataManage
-
+#from common.reflection_tools import is_data_name_acceptable, get_instant_object
+from proto_data_interface_py.BasicDataManage_pb2 import DataResponse, Error, Level
+from logging import Logger
+import datetime
 
 class MongoDataManage(BasicDataManage):
     def __init__(self):
         self.mongo_client = MongoClient('localhost', 27017)
+        db = self.mongo_client.test_database    #建库
+        collection = db.test_collection    #建集合
+        posts = db.posts   #插入文件
 
     def save_detail(self, type_name, to_save_object, insert_optionals):
+        post = {
+            'typename':type_name,
+            'to_save_object':to_save_object,
+            'insert_optionals':insert_optionals
+        }
+        posts = self.db.posts
+        post_id = posts.insert_one(post).inserted_id
         print("pretended to invoke")
--- PoliticManager/test/data_proxy_test/mongo/mongo_client.py
+++ PoliticManager/test/data_proxy_test/mongo/mongo_client.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+#coding=utf-8
 import unittest
 
 from data_proxy_test import basic
