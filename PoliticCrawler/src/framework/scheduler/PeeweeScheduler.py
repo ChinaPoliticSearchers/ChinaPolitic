1cdad63582ee0d5bff4f6521119759062fe9f1bc
DownloadTrumpFans <DownloadTrumpFans@outlook.com>
2017-01-05 00:15:29 +0800

    delete virutal env

--- PoliticCrawler/src/framework/scheduler/PeeweeScheduler.py
+++ PoliticCrawler/src/framework/scheduler/PeeweeScheduler.py
@@ -15,7 +15,8 @@ DEFAULT_PARAMS = {
 def get_sql_settings_from(settings):
     default_params = DEFAULT_PARAMS.copy()
     for key,value in settings.iteritems():
-        default_params.update(settings.get())
+        default_params.update(settings.getdict("REDIS_PARAMS"))
+
 
 
 class PeeweePriorityQueue(object):
@@ -84,101 +85,8 @@ class Scheduler(object):
                 kwargs[name] = val
 
 
-class RedisScheduler(object):
-    @classmethod
-    def from_settings(cls, settings):
-        kwargs = {
-            'persist': settings.getbool('SCHEDULER_PERSIST'),
-            'flush_on_start': settings.getbool('SCHEDULER_FLUSH_ON_START'),
-            'idle_before_close': settings.getint('SCHEDULER_IDLE_BEFORE_CLOSE'),
-        }
 
-        # If these values are missing, it means we want to use the defaults.
-        optional = {
-            # TODO: Use custom prefixes for this settings to note that are
-            # specific to scrapy-redis.
-            'queue_key': 'SCHEDULER_QUEUE_KEY',
-            'queue_cls': 'SCHEDULER_QUEUE_CLASS',
-            'dupefilter_key': 'SCHEDULER_DUPEFILTER_KEY',
-            # We use the default setting name to keep compatibility.
-            'dupefilter_cls': 'DUPEFILTER_CLASS',
-            'serializer': 'SCHEDULER_SERIALIZER',
-        }
-        for name, setting_name in optional.items():
-            val = settings.get(setting_name)
-            if val:
-                kwargs[name] = val
+        def __init__database__(self):
+            pass
 
-        # Support serializer as a path to a module.
-        if isinstance(kwargs.get('serializer'), six.string_types):
-            kwargs['serializer'] = importlib.import_module(kwargs['serializer'])
 
-        server = connection.from_settings(settings)
-        # Ensure the connection is working.
-        server.ping()
-
-        return cls(server=server, **kwargs)
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        instance = cls.from_settings(crawler.settings)
-        # FIXME: for now, stats are only supported from this constructor
-        instance.stats = crawler.stats
-        return instance
-
-    def open(self, spider):
-        self.spider = spider
-
-        try:
-            self.queue = load_object(self.queue_cls)(
-                server=self.server,
-                spider=spider,
-                key=self.queue_key % {'spider': spider.name},
-                serializer=self.serializer,
-            )
-        except TypeError as e:
-            raise ValueError("Failed to instantiate queue class '%s': %s",
-                             self.queue_cls, e)
-
-        try:
-            self.df = load_object(self.dupefilter_cls)(
-                server=self.server,
-                key=self.dupefilter_key % {'spider': spider.name},
-                debug=spider.settings.getbool('DUPEFILTER_DEBUG'),
-            )
-        except TypeError as e:
-            raise ValueError("Failed to instantiate dupefilter class '%s': %s",
-                             self.dupefilter_cls, e)
-
-        if self.flush_on_start:
-            self.flush()
-        # notice if there are requests already in the queue to resume the crawl
-        if len(self.queue):
-            spider.log("Resuming crawl (%d requests scheduled)" % len(self.queue))
-
-    def close(self, reason):
-        if not self.persist:
-            self.flush()
-
-    def flush(self):
-        self.df.clear()
-        self.queue.clear()
-
-    def enqueue_request(self, request):
-        if not request.dont_filter and self.df.request_seen(request):
-            self.df.log(request, self.spider)
-            return False
-        if self.stats:
-            self.stats.inc_value('scheduler/enqueued/redis', spider=self.spider)
-        self.queue.push(request)
-        return True
-
-    def next_request(self):
-        block_pop_timeout = self.idle_before_close
-        request = self.queue.pop(block_pop_timeout)
-        if request and self.stats:
-            self.stats.inc_value('scheduler/dequeued/redis', spider=self.spider)
-        return request
-
-    def has_pending_requests(self):
-        return len(self) > 0
